103a061d090a02ab61f5a2228cdfedfe46efee4b
Sergey M <s.matveyev31@gmail.com>
2023-05-31 17:20:10 +0300

    Add function "CreatCode"

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import "fmt"
 
 func CreatCode(divide string, parts ...string) (code string, err bool) {
+
+	fmt.Println("divide   ", divide)
+	fmt.Println("len(parts)   ", len(parts))
+
 	if len(parts) == 0 {
 		code = "none"
 		err = false
 	} else {
-		for _, part := range parts {
-			code = code + part + divide
+		for i, part := range parts {
+			code = code + part
+			if i < len(parts)-1 {
+				code = code + divide
+			}
 		}
 		err = true
 	}
@@ -19,5 +26,5 @@ func CreatCode(divide string, parts ...string) (code string, err bool) {
 func main() {
 	//CreatCode("29", "VA", "2131")
 
-	fmt.Println(CreatCode("", "29", "VA", "2151"))
+	fmt.Println(CreatCode("-", "29", "VA", "2151"))
 }

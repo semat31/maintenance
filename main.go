40beff9485ffa2ed1644cbf3a939b85e6e24bd80
Sergey M <s.matveyev31@gmail.com>
2023-05-31 16:53:39 +0300

    Code of device

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func CreatCode(divide string, parts ...string) (code string, err bool) {
+	if len(parts) == 0 {
+		code = "none"
+		err = false
+	} else {
+		for _, part := range parts {
+			code = code + part + divide
+		}
+		err = true
+	}
+
+	return code, err
+}
+
 func main() {
-	fmt.Println("Hello Maintenance!!!")
+	//CreatCode("29", "VA", "2131")
+
+	fmt.Println(CreatCode("", "29", "VA", "2151"))
 }

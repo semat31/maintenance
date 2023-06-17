06bc93a98d1da63341cfdcf259d9811d2d00404a
Sergey Matveyev <59117579+semat31@users.noreply.github.com>
2023-06-17 14:06:40 +0000

    code.go: modification-1

--- code/code.go
+++ code/code.go
@@ -1,2 +1,33 @@
+// ***
 package code
 
+import "fmt"
+
+// ***
+type Code string
+
+// ***
+func New(divide string, parts ...string) (Code, bool) {
+
+	fmt.Println("divide   ", divide)
+	fmt.Println("parts   ", parts)
+	fmt.Println("len(parts)   ", len(parts))
+
+	var code string
+	var err bool
+
+	if len(parts) == 0 {
+		code = "none"
+		err = false
+	} else {
+		for i, part := range parts {
+			code = code + part
+			if i < len(parts)-1 {
+				code = code + divide
+			}
+		}
+		err = true
+	}
+
+	return Code(code), err
+}
